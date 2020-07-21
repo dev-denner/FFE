12e9234b1fcc18438ee45d741a2147a848006be3
Denner Fernandes <dev.denner@outlook.com>
2020-07-20 23:29:49 -0300

    feat: aula 05 - function components
    Signed-off-by: Denner Fernandes <dev.denner@outlook.com>

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -4,30 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
- const rootElement = document.getElementById('root');
-// const elementw = document.createElement('div');
-// elementw.innerHTML = 'Hello world!';
-// elementw.className = 'container';
-
-const helloWorld = 'Hello world!';
-const props ={
-  className: 'main-title',
-  children: [ helloWorld, 'Goodbye world'],
-  style:{
-    fontSize: '72px',
-    marginLeft: '10px'
-  }
-};
-const MyComponent = () => <h1 className='my-title' {...props}></h1>
-
-ReactDOM.render(<MyComponent />, rootElement);
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
